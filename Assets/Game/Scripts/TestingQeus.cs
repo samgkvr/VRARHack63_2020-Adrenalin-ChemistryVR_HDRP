4fdfac3db94412cc3eae5b2148e80d2e396c2fee
Christmas <christmas275@users.noreply.github.com>
2020-12-09 14:58:01 +0400

    тесты

--- Assets/Game/Scripts/TestingQeus.cs
+++ Assets/Game/Scripts/TestingQeus.cs
@@ -14,19 +14,25 @@ public class TestingQeus : MonoBehaviour
     [SerializeField] private GameObject Canv3;
     [SerializeField] private GameObject Canv4;
     [SerializeField] private GameObject Canv5;
-    [SerializeField] private GameObject CanvHorocho;
-    [SerializeField] private GameObject CanvPloho;
+
+    [SerializeField] private GameObject Canv11;
+    [SerializeField] private GameObject Canv22;
+    [SerializeField] private GameObject Canv33;
+    [SerializeField] private GameObject Canv44;
+    [SerializeField] private GameObject Canv55;
     public int GameSchet = 0;
     public int YesScet = 0;
     public bool men = false;
 
+    private bool qwe_test;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         GameSchet = 0;
         CanvStart.SetActive(true);
         CanvVopros.SetActive(false);
-        CanvPloho.SetActive(false);
         Canv1.SetActive(false);
         Canv2.SetActive(false);
         Canv3.SetActive(false);
@@ -37,39 +43,102 @@ public class TestingQeus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameSchet == 6 && YesScet == 1)
+        {
+            Canv11.SetActive(true);
+            GameSchet = 1;
+            men = true;
+            GameSchet = 0;
+            YesScet = 0;
+        }
+
+        if (GameSchet == 6 && YesScet == 2)
+        {
+            Canv22.SetActive(true);
+            men = true;
+            GameSchet = 0;
+            YesScet = 0;
+        }
+
+        if (GameSchet == 6 && YesScet == 3)
+        {
+            Canv33.SetActive(true);
+            men = true;
+            GameSchet = 0;
+            YesScet = 0;
+        }
+
+        if (GameSchet == 6 && YesScet == 4)
+        {
+            Canv44.SetActive(true);
+            men = true;
+            GameSchet = 0;
+            YesScet = 0;
+        }
+
+        if (GameSchet == 6 && YesScet == 5)
+        {
+            Canv55.SetActive(true);
+            men = true;
+            GameSchet = 0;
+            YesScet = 0;
+        }
+
 
     }
 
     public void Void2()
     {
-         switch (GameSchet)
+        if (men == true)
         {
-             
-            case 2:
+            men = false;
+            CanvStart.SetActive(true);
+            CanvVopros.SetActive(false);
+            GameSchet = 0;
+            Canv11.SetActive(false);
+            Canv22.SetActive(false);
+            Canv33.SetActive(false);
+            Canv44.SetActive(false);
+            Canv55.SetActive(false);
+        }
+
+
+        switch (GameSchet)
+        {
+         
+            case 1:
                 Canv1.SetActive(false);
                 Canv2.SetActive(true);
+                GameSchet = 2;
+                YesScet = YesScet + 1;
+                break;
+
+            case 2:
+                Canv2.SetActive(false);
+                Canv3.SetActive(true);
                 GameSchet = 3;
                 YesScet = YesScet + 1;
                 break;
+
             case 3:
-                Canv2.SetActive(false);
-                Canv3.SetActive(true);
-                GameSchet = 4;
-                YesScet = YesScet + 1;
-                break;
-            case 4:
                 Canv3.SetActive(false);
                 Canv4.SetActive(true);
-                GameSchet = 5;
                 
+                GameSchet = 4;
+
                 break;
-            case 5:
+            case 4:
                 Canv4.SetActive(false);
                 Canv5.SetActive(true);
                 YesScet = YesScet + 1;
+                GameSchet = 5;
+                break;
+            case 5:
+                Canv5.SetActive(false);
+                GameSchet = 6;
                 break;
         }
-                 
+
     }
     
     private void NextCanv(int num)
@@ -84,38 +153,60 @@ public class TestingQeus : MonoBehaviour
 
     public void Void1()
     {
-        
+        if (men == true)
+        {
+            men = false;
+            Canv1.SetActive(true);
+            Canv11.SetActive(false);
+            Canv22.SetActive(false);
+            Canv33.SetActive(false);
+            Canv44.SetActive(false);
+            Canv55.SetActive(false);
+
+        }
         switch (GameSchet)
         {
             case 0:
                 CanvStart.SetActive(false);
                 CanvVopros.SetActive(true);
                 Canv1.SetActive(true);
-                GameSchet = 2;
+                GameSchet = 1 ;
                 break;
-            case 2:
+
+            case 1:
                 Canv1.SetActive(false);
                 Canv2.SetActive(true);
-                GameSchet = 3;
+                GameSchet = 2;
                 break;
-            case 3:
+
+            case 2:
                 Canv2.SetActive(false);
                 Canv3.SetActive(true);
-                GameSchet = 4;
-                YesScet = YesScet + 1;
-
+                GameSchet = 3;
                 break;
-            case 4:
+
+            case 3:
                 Canv3.SetActive(false);
                 Canv4.SetActive(true);
-                GameSchet = 5;
+                YesScet = YesScet + 1;
+                GameSchet = 4;
+                
                 break;
-            case 5:
+            case 4:
                 Canv4.SetActive(false);
                 Canv5.SetActive(true);
+                
+                GameSchet = 5;
+                break;
+
+            case 5:
+                GameSchet = 6;
                 YesScet = YesScet + 1;
+                Canv5.SetActive(false);
                 break;
         }
+
+       
     }
     
     
